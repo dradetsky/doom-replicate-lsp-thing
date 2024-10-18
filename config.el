54eadcbd3849957732530ac17213418bf1604693
Daniel Radetsky <dradetsky@gmail.com>
2024-10-18 16:34:52 -0700

    init

---
+++ config.el
@@ -0,0 +1,14 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq doom-theme 'doom-one)
+(setq display-line-numbers-type t)
+
+(remove-hook! csharp-mode-local-vars #'lsp)
+(remove-hook! csharp-mode-local-vars-hook #'lsp)
+(remove-hook! 'csharp-mode-local-vars #'lsp)
+(remove-hook! 'csharp-mode-local-vars-hook #'lsp)
+(after! csharp-mode
+  (remove-hook! 'csharp-mode-local-vars #'lsp)
+  (remove-hook! 'csharp-mode-local-vars-hook #'lsp)
+  (remove-hook! csharp-mode-local-vars #'lsp)
+  (remove-hook! csharp-mode-local-vars-hook #'lsp))
