bb4e84cb251e7d911c19f048531fd6a5024fd1d4
Daniel Radetsky <dradetsky@gmail.com>
2024-10-19 02:28:54 -0700

    oops

--- config.el
+++ config.el
@@ -3,20 +3,20 @@
 (setq doom-theme 'doom-one)
 (setq display-line-numbers-type t)
 
-(remove-hook! csharp-mode-local-vars #'lsp)
-(remove-hook! csharp-mode-local-vars-hook #'lsp)
-(remove-hook! 'csharp-mode-local-vars #'lsp)
-(remove-hook! 'csharp-mode-local-vars-hook #'lsp)
+(remove-hook! csharp-mode-local-vars #'lsp!)
+(remove-hook! csharp-mode-local-vars-hook #'lsp!)
+(remove-hook! 'csharp-mode-local-vars #'lsp!)
+(remove-hook! 'csharp-mode-local-vars-hook #'lsp!)
 (after! csharp-mode
-  (remove-hook! 'csharp-mode-local-vars #'lsp)
-  (remove-hook! 'csharp-mode-local-vars-hook #'lsp)
-  (remove-hook! csharp-mode-local-vars #'lsp)
-  (remove-hook! csharp-mode-local-vars-hook #'lsp))
+  (remove-hook! 'csharp-mode-local-vars #'lsp!)
+  (remove-hook! 'csharp-mode-local-vars-hook #'lsp!)
+  (remove-hook! csharp-mode-local-vars #'lsp!)
+  (remove-hook! csharp-mode-local-vars-hook #'lsp!))
 (after! csharp-tree-sitter
-  (remove-hook! 'csharp-mode-local-vars #'lsp)
-  (remove-hook! 'csharp-mode-local-vars-hook #'lsp)
-  (remove-hook! csharp-mode-local-vars #'lsp)
-  (remove-hook! csharp-mode-local-vars-hook #'lsp))
+  (remove-hook! 'csharp-mode-local-vars #'lsp!)
+  (remove-hook! 'csharp-mode-local-vars-hook #'lsp!)
+  (remove-hook! csharp-mode-local-vars #'lsp!)
+  (remove-hook! csharp-mode-local-vars-hook #'lsp!))
 
 ;; i want lsp, but only on-demand.
 (remove-hook! '(c-mode-local-vars-hook
